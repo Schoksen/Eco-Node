f235ea406bfd984bd96bded1d26d84da79e1d554
Schoksen <Schoksen@hotmail.de>
2017-11-13 22:57:06 +0100

    Angular Routing and first Comps

--- ag2-src/src/app/app.module.ts
+++ ag2-src/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ag2-src/src/app/components/profile/profile.component.ts
+++ ag2-src/src/app/components/profile/profile.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class UserComponent implements OnInit {
+export class ProfileComponent implements OnInit {
 
   constructor() { }
 
