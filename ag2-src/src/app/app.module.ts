f0f27329b90b5d01071c0b202d0fde89676e74dc
Schoksen <Schoksen@hotmail.de>
2017-11-13 23:01:28 +0100

    Small Restructuring

--- ag2-src/src/app/app.module.ts
+++ ag2-src/src/app/app.module.ts
@@ -1,7 +1,10 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
+//Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,7 +12,6 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { AppRoutingModule } from './app-routing.module';
 
 
 
